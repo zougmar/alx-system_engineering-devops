094d8e6a8ea9ee787c43ccdd8844bbe460ac9963
zougmar <zougmar123@gmail.com>
2024-09-17 23:30:30 -0700

    c

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,18 @@
+# Using Puppet, install flask from pip3.
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+#instaling werkzeug as a requirement for flask
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
+
